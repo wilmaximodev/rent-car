6103b6d54c45c737eb56080d1732b19ae6580571
wilmaximodev <wilson-max@hotmail.com>
2024-03-05 08:38:05 -0300

    Req 9

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -2,6 +2,10 @@ using RentCars.Types;
 
 namespace RentCars.Models;
 
+// O enum deve conter o campo Confirmed com valor 0
+// O enum deve conter o campo Finished com valor 1
+// O enum deve conter o campo Canceled com valor igual ao Finished
+
 public class Rent
 {
     public Vehicle Vehicle { get; set; }
--- src/RentCars/Types/Enums/RentStatus.cs
+++ src/RentCars/Types/Enums/RentStatus.cs
@@ -1,6 +1,11 @@
 namespace RentCars.Types;
 
-//9 - Crie um enum `RentStatus`
+// O enum deve conter o campo Confirmed com valor 0
+// O enum deve conter o campo Finished com valor 1
+// O enum deve conter o campo Canceled com valor igual ao Finished
 public enum RentStatus
 {
+    Confirmed = 0,
+    Finished = 1,
+    Canceled = 1
 }
