93e54ea3ee9e1f9c51472c4fd0abb03af6408198
wilmaximodev <wilson-max@hotmail.com>
2024-03-05 08:33:45 -0300

    Req 7

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -2,7 +2,13 @@ using RentCars.Types;
 
 namespace RentCars.Models;
 
-//7 - Faça a classe `Motorcycle` herdar de `Vehicle`
+// A classe deve ter as seguintes propriedades:
+// SeatHeight que deve ser do tipo double;
+// FrontBrake que deve ser do tipo BrakeType;
+// RearBrake que deve ser do tipo BrakeType;
 public class Motorcycle : Vehicle
 {
+    public double SeatHeight { get; set; }
+    public BrakeType FrontBrake { get; set; }
+    public BrakeType RearBrake { get; set; }
 }
